ef0256de50a858d9ff143aec64240d5768692ba2
Nguyễn Hữu Duy Đạt <duydat13012003@gmail.com>
2025-06-05 14:25:32 +0700

    fix: sửa lỗi tiếng anh thành tiếng việt trong get lấy tất cả danh sách tài sản

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('xoa_tai_san/<str:id>/', views.xoa_tai_san, name='xoa_tai_san'),
-    path('api/taisan/', views.get_all_taisan, name='get_all_taisan'),
+    path('api/taisan/', views.get_lay_tat_ca_taisan, name='get_all_taisan'),
 ]
