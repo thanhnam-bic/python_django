0fa715e489355612968420c0ab2c5afaeb915c84
23730134-TranMinhTri <165041974+MinhTri00k@users.noreply.github.com>
2025-06-05 11:10:23 +0700

    Cap_nhat_API_xoa_tai_san_theo_ID

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-#Định nghĩa các route ở đây ví dụ 
-#path('index', views.index, name='index'),
-#path('api_nhanvien', views.api_nhanvien, name='api_nhanvien'),
+    path('', views.index, name='index'),
+    path('xoa_tai_san/<str:id>/', views.xoa_tai_san, name='xoa_tai_san'),
 ]
