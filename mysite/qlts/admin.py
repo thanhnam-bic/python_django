06e9cb6ccd8edb974dd1fe6889d45f367ae23b3f
DuyDat13012003 <duydat13012003@gmail.com>
2025-06-07 19:44:05 +0700

    Revert "[Trinh] Thiết lập tài khoản admin "

--- mysite/qlts/admin.py
+++ mysite/qlts/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import TaiSan, NhanVien, DanhMuc, NhaSanXuat, NhaCungCap, ViTri
-# Register your models here.
 
-admin.site.register(TaiSan)
-admin.site.register(NhanVien)
-admin.site.register(DanhMuc)
-admin.site.register(NhaSanXuat)
-admin.site.register(NhaCungCap)
-admin.site.register(ViTri)
+# Register your models here.
