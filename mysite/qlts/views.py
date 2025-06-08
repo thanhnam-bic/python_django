aafc5d9e3e01b7147cba56889190695845ac0231
23730134-TranMinhTri <165041974+MinhTri00k@users.noreply.github.com>
2025-06-08 19:21:18 +0700

    fix: API chi_tiet_tai_san

--- mysite/qlts/views.py
+++ mysite/qlts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from .models import TaiSan
-import json
 from django.views.decorators.csrf import csrf_exempt
 
 # API IndexIndex
@@ -45,27 +44,28 @@ def chi_tiet_tai_san(request, id):
     if request.method == 'GET':
         try:
             asset = TaiSan.objects.get(pk=id)
+            du_lieu = {
+                "ma_tai_san": asset.ma_tai_san,
+                "ten_tai_san": asset.ten_tai_san,
+                "so_serial": asset.so_serial,
+                "gia_mua": float(asset.gia_mua),
+                "danh_muc": {
+                    "ten_danh_muc": asset.danh_muc.danh_muc if asset.danh_muc else None
+                },
+                "nhan_vien": {
+                    "ma_nhan_vien": asset.ma_nhan_vien.ma_nhan_vien if asset.ma_nhan_vien else None
+                },
+                "nha_san_xuat": {
+                    "ten_nha_san_xuat": asset.nha_san_xuat.nha_san_xuat if asset.nha_san_xuat else None
+                },
+                "nha_cung_cap": {
+                    "ten_nha_cung_cap": asset.nha_cung_cap.nha_cung_cap if asset.nha_cung_cap else None
+                }
+            }
             return JsonResponse({
                 "thanh_cong": True,
-                "thong_bao": "Lấy thông tin tài sản thành công.",
-                "du_lieu": {
-                    "ma_tai_san": asset.ma_tai_san,
-                    "ten_tai_san": asset.ten_tai_san,
-                    "so_serial": asset.so_serial,
-                    "gia_mua": float(asset.gia_mua),
-                    "danh_muc": {
-                        "ten_danh_muc": asset.danh_muc.ten_danh_muc if asset.danh_muc else None
-                    },
-                    "nhan_vien": {
-                        "ma_nhan_vien": asset.ma_nhan_vien.ma_nhan_vien if asset.ma_nhan_vien else None
-                    },
-                    "nha_san_xuat": {
-                        "ten_nha_san_xuat": asset.nha_san_xuat.ten_nha_san_xuat if asset.nha_san_xuat else None
-                    },
-                    "nha_cung_cap": {
-                        "ten_nha_cung_cap": asset.nha_cung_cap.ten_nha_cung_cap if asset.nha_cung_cap else None
-                    }
-                }
+                "thong_bao": "Lấy chi tiết tài sản thành công",
+                "du_lieu": du_lieu
             })
         except TaiSan.DoesNotExist:
             return JsonResponse({
@@ -78,8 +78,5 @@ def chi_tiet_tai_san(request, id):
     else:
         return JsonResponse({
             "success": False,
-            "message": "Không hỗ trợ phương thức này.",
-            "errors": {
-                "method": "Chỉ hỗ trợ phương thức GET"
-            }
+            "message": "Chỉ hỗ trợ phương thức GET."
         }, status=405)
