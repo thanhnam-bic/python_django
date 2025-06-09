f17330043ef30076c3070d3c2051b10fd089fa1c
23730134-TranMinhTri <165041974+MinhTri00k@users.noreply.github.com>
2025-06-09 09:52:50 +0700

    fear: add API PUT cap_nhat_tai_san

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/xoa_tai_san/<str:id>/', views.xoa_tai_san, name='xoa_tai_san'),
+    path('api/cap_nhat_tai_san/<str:id>/', views.cap_nhat_tai_san, name='cap_nhat_tai_san'),
     path('api/taisan/', views.get_tat_ca_taisan, name='get_all_taisan'),
     path('api/taisan/nhanvien/', views.tinh_taisan_moi_nhanvien, name='tinh_taisan_moi_nhanvien'),
 ]
