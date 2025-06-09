dc40766cf8e2e1f8afe111aabd05d6b8ee0b8887
23730134-TranMinhTri <165041974+MinhTri00k@users.noreply.github.com>
2025-06-09 09:33:23 +0700

    fear: add API GET chi_tiet_tai_san_ID

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/xoa_tai_san/<str:id>/', views.xoa_tai_san, name='xoa_tai_san'),
+    path('api/chi_tiet_tai_san/<str:id>/', views.chi_tiet_tai_san, name='chi_tiet_tai_san'),
     path('api/taisan/', views.get_tat_ca_taisan, name='get_all_taisan'),
     path('api/taisan/nhanvien/', views.tinh_taisan_moi_nhanvien, name='tinh_taisan_moi_nhanvien'),
 ]
