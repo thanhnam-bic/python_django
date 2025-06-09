52064523e165a42bea409dcb4da8baba51d62aad
Nguyễn Hữu Duy Đạt <duydat13012003@gmail.com>
2025-06-09 11:06:53 +0700

    feat:post merge cái này sau

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/xoa_tai_san/<str:id>/', views.xoa_tai_san, name='xoa_tai_san'),
-    path('api/taisan/', views.get_tat_ca_taisan, name='get_all_taisan'),
+    path('api/taisan/', views.get_tat_ca_taisan, name='lay_tat_ca_tai_san'),
     path('api/taisan/nhanvien/', views.tinh_taisan_moi_nhanvien, name='tinh_taisan_moi_nhanvien'),
+    path('api/taisan/add/', views.tao_tai_san, name='tao_tai_san'),
 ]
