e700d291d1592537e44c47e644e8c9c0595e92df
trinhnguyen <113288208+trinhnguyen2k@users.noreply.github.com>
2025-06-14 17:25:00 +0700

    [Trinh] Feat: Thêm unit test cho các API và thêm Module GET thông kê tài sản của nhân viên (#29)
    * add unit test cho cac api
    
    * them tet cho api thong ke tai san
    
    * thêm api tính tài san cua nhan vien
    
    * tich hop tinh tai san API vao file View
    
    * setup url cho api tinh tai san nhan vien

---
+++ mysite/qlts/qlts_thong_ke_tai_san_nhan_vien/__init__.py
@@ -0,0 +1,9 @@
+"""
+File: __init__.py
+Thư mục: qlts/Thong_ke_tai_san_nhan_vien/
+Mục đích: Biến thư mục thành Python package
+"""
+
+# Import các hàm chính để dễ dàng truy cập
+from .qlts_thong_ke_tai_san_nhan_vien import TinhTaiSanNhanVien
+__all__ = ['thong_ke_tai_san_nhan_vien']
