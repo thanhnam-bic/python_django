b7fcd474f045b62ee3d0092b473e3c3e1a138f58
trinhnguyen2k <nguyennhatthuytrinh1311@gmail.com>
2025-06-14 17:20:13 +0700

    setup url cho api tinh tai san nhan vien

--- mysite/qlts/urls.py
+++ mysite/qlts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('api/cap_nhat_tai_san/<str:id>/', views.cap_nhat_tai_san, name='cap_nhat_tai_san'),
     path('api/chi_tiet_tai_san/<str:id>/', views.chi_tiet_tai_san, name='chi_tiet_tai_san'),
     path('api/lay_tat_ca_tai_san/', views.lay_tat_ca_tai_san, name='lay_tat_ca_tai_san'),
-    path('api/tinh_tai_san_moi_nhan_vien/', views.tinh_tai_san_moi_nhan_vien, name='tinh_tai_san_moi_nhan_vien'),
     path('api/tao_tai_san/',views.tao_tai_san,name='tao_tai_san'),
+    path('api/tinh_tai_san_nhan_vien/', views.tinh_tai_san_nhan_vien, name='tinh_tai_san_nhan_vien'),
 
 ]
